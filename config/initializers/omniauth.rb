4488468bcc39d27c0ad5a3e45da49b0c26d88ae1
e601201 <k.arthur1111@gmail.com>
2023-11-14 16:44:50 +0900

    credentialのkeycloakの追記

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :keycloak_openid, 'demo-client', 'AN8p731FGs0aCFlcmCUMT7NW3n7qvdbp',
-    client_options: {base_url: '', site: 'http://localhost:8080',
-    realm: 'demo-realm'}, name: 'keycloak'
+  keycloak = Rails.application.credentials.keycloak
+  provider :keycloak_openid,
+  keycloak[:client],
+  keycloak[:secretkey],
+  client_options: {
+    base_url: '', site: keycloak[:domain], realm: keycloak[:realm]
+  },
+  name: 'keycloak'
 end
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
